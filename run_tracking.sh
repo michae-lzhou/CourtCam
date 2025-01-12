800e88b42569e005a9c553b81134cd98ca9245fc
Michael Zhou <ziyang.zhou@gmail.com>
2025-01-11 19:47:40 -0800

    Updated run_tracking.sh

--- run_tracking.sh
+++ run_tracking.sh
@@ -8,14 +8,28 @@ first time you run it)'
 pip install -r bball_game_tracking/requirements.txt > /dev/null 2>&1
 echo 'Done!'
 
+# Set nullglob to handle no matches correctly
+shopt -s nullglob
+shopt -s nocaseglob
+
 # Find all .mp4 files in the current directory
 mp4_files=(*.mp4)
-echo "Found ${#mp4_files[@]} .mp4 files."
-echo "${mp4_files}"
-sleep 1
+mp4_count=${#mp4_files[@]}
+
+# Reset nullglob to its default state
+shopt -u nullglob
+shopt -u nocaseglob
+
+# Check if any MP4 files exist
+if [ $mp4_cound -eq 0 ]; then
+  echo "No .mp4 files found in the current directory. Please try again"
+  echo "Press any key to exit"
+  read -n 1
+  exit 1
+fi
 
 # Check if exactly one .mp4 file exists
-if [ ${#mp4_files[@]} -eq 1 ]; then
+if [ $mp4_count -eq 1 ]; then
   # Open a new terminal window and run the Python script
   if command -v gnome-terminal &> /dev/null; then
     gnome-terminal -- bash -c "python3 -m bball_game_tracking.main '${mp4_files[0]}'; echo 'Press any key to exit'; read -n 1"
